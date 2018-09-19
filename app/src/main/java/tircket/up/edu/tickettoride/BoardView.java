74c24bb6368f94f555a56bb8436f63cc604dcdf2
Lexden12 <lexden.s@gmail.com>
2018-09-19 09:08:43 -0700

    Add BoardView and prepare to draw board in BoardView of activity.

--- app/src/main/java/tircket/up/edu/tickettoride/BoardView.java
+++ app/src/main/java/tircket/up/edu/tickettoride/BoardView.java
@@ -1,4 +1,42 @@
 package tircket.up.edu.tickettoride;
 
-public class BoardView {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class BoardView extends SurfaceView{
+    private Bitmap bmp;
+    Rect src = new Rect();
+    Rect dest = new Rect();
+    Context context;
+    Point size;
+    public BoardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setWillNotDraw(false);
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.board);
+        this.context = context;
+    }
+
+    public void setSize(Point point){
+        size = point;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        src.top = 0;
+        src.left = 0;
+        src.bottom = bmp.getHeight();
+        src.right = bmp.getHeight();
+        dest.top = 0;
+        dest.left = 0;
+        dest.bottom = size.y-1;
+        dest.right = size.x-1;
+        canvas.drawBitmap(bmp, src, dest, null);
+    }
 }
--- app/src/main/java/tircket/up/edu/tickettoride/MainActivity.java
+++ app/src/main/java/tircket/up/edu/tickettoride/MainActivity.java
@@ -1,13 +1,18 @@
 package tircket.up.edu.tickettoride;
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    BoardView boardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+        boardView = findViewById(R.id.boardView);
+        boardView.setSize(point);
     }
 }
