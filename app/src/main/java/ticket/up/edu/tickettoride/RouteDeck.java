73b617e580c06f04bb618dda754991c5a0f39cd5
Cameron94857 <beillard21@up.edu>
2018-09-20 21:50:39 -0700

    -Modified Route Deck; still needs updated resource references -TrainButton modified -TrainStash created

--- app/src/main/java/ticket/up/edu/tickettoride/RouteDeck.java
+++ app/src/main/java/ticket/up/edu/tickettoride/RouteDeck.java
@@ -1,4 +1,44 @@
 package ticket.up.edu.tickettoride;
 
+import android.content.Context;
+import android.graphics.BitmapFactory;
+
 public class RouteDeck extends Deck {
+
+    public RouteDeck(Context context) {
+        /*Card[] routeCards = { //need card images in resources to update names (30 routes total)
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York"),
+            new Card(BitmapFactory.decodeResource(context.getResources(), R.drawable.new_york), "New York")
+        };*/
+
+        shuffle();
+    }
 }
--- app/src/main/java/ticket/up/edu/tickettoride/TrainButton.java
+++ app/src/main/java/ticket/up/edu/tickettoride/TrainButton.java
@@ -2,22 +2,26 @@ package ticket.up.edu.tickettoride;
 
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
 
 public class TrainButton {
 
-    int topLeft;
-    int topRight;
-    int bottomLeft;
-    int bottomRight;
+    Point topLeft;
+    Point topRight;
+    Point bottomLeft;
+    Point bottomRight;
     Color color;
     boolean highlight;
+    boolean visible;
 
-    public TrainButton(int topLeft, int topRight, int bottomLeft, int bottomRight, Color trainColor){
+    public TrainButton(Point topLeft, Point topRight, Point bottomLeft, Point bottomRight, Color trainColor){
         this.topLeft = topLeft;
         this.topRight = topRight;
         this.bottomLeft = bottomLeft;
         this.bottomRight = bottomRight;
         this.color = trainColor;
+        this.visible = true;
     }
 
     public void drawTrain(Canvas canvas){
@@ -30,5 +34,14 @@ public class TrainButton {
 
     public void setHighlight(boolean highlight){
         this.highlight = highlight;
+    }
+
+    public void draw(Canvas c){
+        Paint trainPaint = new Paint();
+        trainPaint.setColor(Color.GREEN); //temporary
+        c.drawLine(topLeft.x, topLeft.y, topRight.x, topRight.y, trainPaint);
+        c.drawLine(topLeft.x, topLeft.y, bottomLeft.x, bottomRight.y, trainPaint);
+        c.drawLine(bottomLeft.x, bottomLeft.y, bottomRight.x, bottomRight.y, trainPaint);
+        c.drawLine(bottomRight.x, bottomRight.y, topRight.x, topRight.y, trainPaint);
     }
 }
